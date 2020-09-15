17bc5c7d047e8b6ffeb323c28ff6f2c9b8bc0f21
barattoM <marvin.baratto@gmail.com>
2020-09-15 09:12:22 +0200

    15/09/20 gitignore

---
+++ 01- PHP CLI/exercice4-5.php
@@ -0,0 +1,16 @@
+<?php
+    $age=readline("Donnez votre age : ");
+    $sexe=readline("Donnez votre sexe : ");
+    // if(($sexe="homme" && $age>20) || ($sexe="femme" && $age>18 && $age<35)){
+    //     echo "Imposable";
+    // }
+    // else{
+    //     echo "Pas imposable";
+    // }
+    
+    if($sexe="homme"){
+
+    }
+    else{
+        
+    }
--- 01- PHP CLI/exercice4.5.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $age=readline("Donnez votre age : ");
-    $sexe=readline("Donnez votre sexe : ");
-    if($sexe="homme"){
-
-    }
-    else{
-        
-    }
