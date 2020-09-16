4ce7c5fbb24d0cf23c92c695fe60c8fb0178af12
barattoM <marvin.baratto@gmail.com>
2020-09-16 17:24:12 +0200

    16/09/20 soir

--- 01- PHP CLI/exercice5-6.php
+++ 01- PHP CLI/exercice5-6.php
@@ -12,13 +12,11 @@ if($nombre==0 || $nombre==1){
     echo "La factorielle de ".$nombre." est 1";
 }
 else {
-    $phrase=" 0 X 1";
+    $phrase="1";
     $fact=1;
     for($i=2;$i<=$nombre;$i++){
         $fact=$fact*$i;
         $phrase=$phrase." X ".$i;
-        echo $phrase;
+        echo $phrase." = ".$fact."\n";
     }
-    $phrase=$phrase." = ".$fact;
-    echo $phrase;
 }
