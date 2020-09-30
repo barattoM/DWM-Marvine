adce93c0ffc79beaa44d5e92dad3b9984c1d55fe
59011-95-14 <59011-95-14@exchange.ad.afpanet>
2020-09-30 17:28:25 +0200

    30/09/2020 soir

--- 01- PHP CLI/Morpion/Morpion.php
+++ 01- PHP CLI/Morpion/Morpion.php
@@ -1,16 +1,26 @@
 <?php
+/**
+ * Compte le nombre de symboles alignés successifs dans une direction à partir d'une position (cette position n'est pas comptée)
+ *
+ * @param [array] $plateau      Plateau de jeu
+ * @param [array] $positions    La position à partir de laquelle compter
+ * @param [int] $directionX     Direction sur les lignes
+ * @param [int] $directionY     Direction sur les colonnes
+ * @param [char] $symbole       Symbole du joueur actuel
+ * @return int  Renvoie le nombre de symbole successif en suivant la direction donnée
+ */
 function compteAlignes($plateau,$positions,$directionX,$directionY,$symbole){
     $positionSuivanteX=$positions[0]+$directionX;
     $positionSuivanteY=$positions[1]+$directionY;
-    $symboleSuivant=$plateau[$positionSuivanteX][$positionSuivanteY];
     $positions[0]=$positionSuivanteX;
     $positions[1]=$positionSuivanteY;
-    if($symboleSuivant!=$symbole || ){ //on s'arrête quand le symbole suivant est différent ou que la position suivante n'est pas dans le tableau
+    if($positions[0]>=count($plateau) || $positions[1]>=count($plateau[0] || $positions[0]<0) || $positions[1]<0){ //vérification si on se trouve toujours dans le tableau, si non on arrête
+        return 0;
+    }
+    $symboleSuivant=$plateau[$positionSuivanteX][$positionSuivanteY];   
+    if($symboleSuivant!=$symbole){ //on s'arrête quand le symbole suivant est différent
         return 0;
     }else{              
-            $nbSymboleAligne+=compteAlignes($plateau,$positions,$directionX,$directionY,$symbole);
-            return $nbSymboleAligne;
-        
-        
+        return 1+compteAlignes($plateau,$positions,$directionX,$directionY,$symbole);        
     }
 }
