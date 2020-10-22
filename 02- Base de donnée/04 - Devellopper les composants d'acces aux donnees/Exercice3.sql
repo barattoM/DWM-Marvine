1118ad20e5eb814e32b240c04de90d4785419cad
59011-95-14 <59011-95-14@exchange.ad.afpanet>
2020-10-22 12:25:26 +0200

    22/10/2020 matin

--- 02- Base de donnée/04 - Devellopper les composants d'acces aux donnees/Exercice3.sql
+++ 02- Base de donnée/04 - Devellopper les composants d'acces aux donnees/Exercice3.sql
@@ -31,12 +31,21 @@ E) Le nom des étudiants qui partagent une ou plusieurs notes avec « LUC DUPONT
 notes de LUC DUPONT:
 SELECT note FROM `etudiants` INNER JOIN avoir_note ON avoir_note.idEtudiant = etudiants.idEtudiant INNER JOIN epreuves ON epreuves.idEpreuve = avoir_note.idEpreuve WHERE prenomEtudiant = "LUC" AND nomEtudiant = "DUPONT" 
 
-
+Final :
+SELECT `nomEtudiant`, `prenomEtudiant` FROM `etudiants` INNER JOIN avoir_note ON avoir_note.idEtudiant = etudiants.idEtudiant WHERE note IN( SELECT note FROM `etudiants` INNER JOIN avoir_note ON avoir_note.idEtudiant = etudiants.idEtudiant INNER JOIN epreuves ON epreuves.idEpreuve = avoir_note.idEpreuve WHERE prenomEtudiant = "LUC" AND nomEtudiant = "DUPONT" ) 
 
 F) Ajoutez une colonne HOBBY à la table ETUDIANT. Cette colonne est de type chaine sur 20 caractères.
 Par défaut le HOBBY est mis à SPORT. 
+
+ALTER TABLE etudiant ADD hobby VARCHAR(20);
+ALTER TABLE `etudiants` ADD `hobby` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'SPORT' AFTER `dateNaissanceEtudiant`; 
+
 G) Ajouter à la table ETUDIANT une colonne NEWCOL de type INTEGER (vérifier en affichant la
 structure de la table) puis la supprimer (vérifier de nouveau la suppression).
+
+ALTER TABLE `etudiants` ADD `NEWCOL` INT CHARACTER SET utf8 COLLATE utf8_general_ci AFTER `hobby`;
+ALTER TABLE `etudiants` DROP `NEWCOL`;
+
 H) Vérifiez que PREnomEtudiant peut ne pas avoir de contenu puis indiquez que la colonne PREnomEtudiant
 doit obligatoirement avoir une valeur. Vérifiez sur la description de la table.
 I)Insérez les enregistrements suivants: 7, 'interro écrite',9,1,'21-oct-96',1 dans EPREUVE 
@@ -45,7 +54,12 @@ I)Insérez les enregistrements suivants: 7, 'interro écrite',9,1,'21-oct-96',1 
 3,7,05
 4,7,09 
 17,3,15 dans AVOIR_NOTE
+
+
 J) Changez la note de n°3 dans l épreuve7, elle passe à 07. Vérifiez les valeurs avant et après la requête.
+
+UPDATE avoir_note SET note=07 WHERE idAvoirNote= (SELECT idAvoirNote FROM (SELECT * FROM avoir_note WHERE idEpreuve=7) as a LIMIT 2,1 )
+
 K) N°1 a mis de la bonne volonté, on augmente sa note dans l épreuve 7 de 1 point. Vérifiez.
 L) Détruisez l épreuve 7 qui a été annulée pour cause de tricherie et les notes qui lui correspondent. Vérifiez.
 M)Réflexion : N y aurait-il pas eu moyen de détruire les notes de l épreuve automatiquement dès la destruction de l épreuve ?
