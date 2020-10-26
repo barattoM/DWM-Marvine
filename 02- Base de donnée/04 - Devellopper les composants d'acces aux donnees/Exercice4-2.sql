3646192812d365ddd4e31010c3dba50d7c5c2e1e
barattoM <marvin.baratto@gmail.com>
2020-10-26 12:29:28 +0100

    26/10/2020 midi

--- 02- Base de donnée/04 - Devellopper les composants d'acces aux donnees/Exercice4-2.sql
+++ 02- Base de donnée/04 - Devellopper les composants d'acces aux donnees/Exercice4-2.sql
@@ -1,12 +1,39 @@
 1. Rechercher le prénom des employés et le numéro de la région de leur département.
+
+SELECT `prenom`,`noregion` FROM `employe` INNER JOIN dept ON employe.nodep=dept.nodept 
+
 2.Rechercher le numéro du département, le nom du département, le nom des employés classés par numéro de département (renommer les tables utilisées)
+
+SELECT `nodept`,dept.`nom` as "Nom du département",employe.nom FROM `dept` INNER JOIN employe ON employe.nodep=dept.nodept ORDER BY `nodept` 
+
 3.Rechercher le nom des employés du département Distribution. 
+
+SELECT employe.nom FROM `dept` INNER JOIN employe ON employe.nodep=dept.nodept WHERE dept.`nom`="Distribution" 
+
 4.Rechercher le nom et le salaire des employés qui gagnent plus que leur patron, et le nom et le salaire de leur patron
+
+SELECT `nom` AS "Nom Employé", `salaire` AS "Salaire Employé", (SELECT `nom` FROM employe WHERE `titre`="président")AS "Nom Supérieur", (SELECT `salaire` FROM employe WHERE `titre`="président")AS "Salaire supérieur" FROM `employe` WHERE `salaire` >(SELECT `salaire` FROM employe WHERE `titre`="président") 
+
 5.Rechercher le nom et le titre des employés qui ont le même titre que Amartakaldire.
+
+SELECT `nom`,`titre` FROM `employe` WHERE `titre`=(SELECT `titre` FROM employe WHERE nom="Amartakaldire")
+
 6.Rechercher le nom, le salaire et le numéro de département des employés qui gagnent plus qu un seul employé du département 31, classés par numéro de département et salaire. 
+
+
+
 7.Rechercher le nom, le salaire et le numéro de département des employés qui gagnent plus que tous les employés du département 31, classés par numéro de département et salaire. 
+
+
+
 8.Rechercher le nom et le titre des employés du service 31qui ont un titre que l on trouve dans le département 32.
+
+
+
 9.Rechercher le nom et le titre des employés du service 31qui ont un titre l on ne trouve pas dans le département 32.
+
+
+
 10.Rechercher le nom, le titre et le salaire des employés qui ont le même titre et le même salaire que Fairant. 
 11.Rechercher le numéro de département, le nom du département, le nom des employés, en affichant aussi les départements dans lesquels il n y a personne, classés par nuéro de département.
 12.Calculer le nombre d employés de chaque titre.
