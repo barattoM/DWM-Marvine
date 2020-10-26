6dd630d5c79b6d8352e10bd5a5447b07d57aa178
barattoM <marvin.baratto@gmail.com>
2020-10-26 08:45:45 +0100

    26/10/2020 8:45

---
+++ 02- Base de donnée/04 - Devellopper les composants d'acces aux donnees/Exercice4-1.sql
@@ -0,0 +1,61 @@
+1- Afficher toutes les informations concernant les employé
+
+2- Afficher toutes les informations concernant les départements
+
+3-Afficher le nom, la date d embauche le numero du supérieur, le numéro de départements et le salaire de tous les employés
+
+4- Afficher le titre de tous les employés
+
+5- Afficher les différntes valeurs des titres des employés
+
+6. Afficher le nom, le numéro d employé et le numéro du département des employés dont le titre est «Secrétaire».
+
+7. Afficher le nom et le numéro de département dont le numéro de département est supérieur à 40
+
+8.Afficher le nom et le prénom des employés dont le nom est alphabétiquement antérieur au prénom.
+
+9.Afficher le nom, le salaire et le numéro du département des employés dont le titre est «Représentant», le numéro de département est 35et le salaire est supérieur à 20000.
+
+10.Afficher le nom, le titre et le salaire des employés dont le titre est «Représentant» ou dont le titre est «Président».
+
+11.Afficher le nom, le titre, le numéro de département, le salaire des employés du département 34, dont le titre est «Représentant» ou «Secrétaire». 
+
+12.Afficher le nom, le titre, le numéro de département, le salaire des employés dont le titre est Représentant, ou dont le titre est Secrétaire dans le département numéro 34. 
+
+13.Afficher le nom, et le salaire des employés dont le salaire est compris entre 20000et 30000.
+
+15.Afficher le nom des employés commençant par la lettre «H». 
+
+16.Afficher le nom des employés se terminant par la lettre «n». 
+
+17.Afficher le nom des employés contenant la lettre «u» en 3èmeposition. 
+
+18.Afficher le salaire et le nom des employés du service 41classés par salaire croissant. 
+
+19.Afficher le salaire et le nom des employés du service 41classés par salaire décroissant. 
+
+20.Afficher le titre, le salaire et le nom des employés classés par titre croissant et par salaire décroissant. 
+
+21.Afficher le taux de commission, le salaire et le nom des employés classés par taux de commission croissante.
+
+22.Afficher le nom, le salaire, le taux de commission et le titre des employés dont le taux de commission n est pas renseigné. 
+
+23.Afficher le nom, le salaire, le taux de commission et le titre des employés dont le taux de commission est renseigné.
+
+24.Afficher le nom, le salaire, le taux de commission, le titre des employés dont le taux de commission est inférieur à 15. 
+
+25.Afficher le nom, le salaire, le taux de commission, le titre des employés dont le taux de commission est supérieur à 15
+
+26.Afficher le nom, le salaire, le taux de commission et la commission des employés dont le taux de commission n est pas nul.(la commission est calculée en multipliant le salaire par le taux de commission)
+
+27.Afficher le nom, le salaire, le taux de commission, la commission des employés dont le taux de commission n est pas nul, classé par taux de commission croissant. 
+
+28. Afficher le nom et le prénom (concaténés) des employés. Renommer les colonnes
+
+29.Afficher les 5 premières lettres du nom des employés. 
+
+30. Afficher le nom et le rang de la lettre«r» dans le nom des employés. 
+
+31. Afficher le nom, le nom en majuscule et le nom en minuscule de l employé dont le nom est Vrante
+
+32.Afficher le nom et le nombre de caractères du nom des employés. 
