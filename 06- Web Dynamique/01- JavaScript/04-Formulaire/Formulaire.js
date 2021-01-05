f416db8a31b88cc1f683cc41f532e550b169e6cb
barattoM <marvin.baratto@gmail.com>
2021-01-05 17:19:09 +0100

    05/01/2021

--- 06- Web Dynamique/01- JavaScript/04-Formulaire/Formulaire.js
+++ 06- Web Dynamique/01- JavaScript/04-Formulaire/Formulaire.js
@@ -4,6 +4,10 @@ var postal=document.getElementById("postal");
 var erreurPostal=document.getElementById("erreurPostal");
 var DDN=document.getElementById("DDN");
 var erreurDDN=document.getElementById("erreurDDN");
+var mail=document.getElementById("mail");
+var erreurMail=document.getElementById("erreurMail");
+var mdp=document.getElementById("mdp");
+var erreurMdp=document.getElementById("erreurMdp");
 var submit=document.getElementById("submit");
 var imgConseils=document.getElementsByClassName("imgConseil");
 var iconeErreurs=document.getElementsByClassName("imgErreur");
@@ -11,9 +15,12 @@ var iconeErreurs=document.getElementsByClassName("imgErreur");
 var flagNom=false;
 var flagPostal=false;
 var flagDDN=false;
+var flagMail=false;
+var flagMdp=false;
 
 nom.addEventListener("blur",function(){
-    if(nom.value.length<3){
+    //!/^[A-Za-z-' ]{3,}$/.test(nom.value)
+    if(!nom.checkValidity()){
         erreurNom.textContent="Le nom doit être renseigné";
         iconeErreurs[0].setAttribute("src",'./uncheck.png');
         flagNom=false;
@@ -38,7 +45,8 @@ nom.addEventListener("keyup",function(e){
 });
 
 postal.addEventListener("blur",function(e){
-    if(postal.value.length!=5){
+    //!/^\d{5}$/.test(postal.value)
+    if(!postal.checkValidity()){
         erreurPostal.textContent="Le code postal doit contenir 5 caractères numériques";
         iconeErreurs[1].setAttribute("src",'./uncheck.png');
         flagPostal=false;      
@@ -57,7 +65,8 @@ postal.addEventListener("keyup",function(e){
 });
 
 DDN.addEventListener("blur",function(){
-    if(DDN.value.length==0){
+    //!/^[1-2][0-9][0-9][0-9]\-[0-1]?[0-9]\-[0-3]?[0-9]$/.test(DDN.value)
+    if(!DDN.checkValidity()){
         erreurDDN.textContent="La date doit être renseigné";
         iconeErreurs[2].setAttribute("src",'./uncheck.png');
         flagDDN=false;     
@@ -69,7 +78,41 @@ DDN.addEventListener("blur",function(){
     verif();
 });
 
+mail.addEventListener("blur",function(){
+    //!/^[\w-]*\.?[\w-]+@[\w]+\.[a-z]{2,4}$/.test(mail.value)
+    if(!mail.checkValidity()){
+        erreurMail.textContent="L'adresse mail n'est pas valide";
+        iconeErreurs[3].setAttribute("src",'./uncheck.png');
+        flagMail=false;     
+    }else{
+        erreurMail.textContent="";
+        flagMail=true;
+        iconeErreurs[3].setAttribute("src",'./check.png');
+    }
+    verif();
+});
 
+mdp.addEventListener("input",function(e){
+    //!/^\d{5}$/.test(Mdp.value)
+    if(!mdp.checkValidity()){
+        erreurMdp.textContent="Le mode de passe est invalide";
+        iconeErreurs[4].setAttribute("src",'./uncheck.png');
+        flagMdp=false;      
+    }else{
+        erreurMdp.textContent="";
+        flagMdp=true;
+        iconeErreurs[4].setAttribute("src",'./check.png');
+    }
+    verif();
+});
+
+mdp.addEventListener("mouseover",function(e){
+    e.target.setAttribute("type","text");
+});
+
+mdp.addEventListener("mouseout",function(e){
+    e.target.setAttribute("type","password");
+});
 
 for(let i=0;i<imgConseils.length;i++){
     imgConseils[i].addEventListener("mouseover",function(e){
@@ -86,7 +129,7 @@ for(let i=0;i<imgConseils.length;i++){
 
 
 function verif(){
-    if(flagPostal && flagDDN && flagNom){
+    if(flagPostal && flagDDN && flagNom && flagMail && flagMdp){
         submit.removeAttribute("disabled");
     }
     else{
--- 06- Web Dynamique/01- JavaScript/04-Formulaire/Formulaire.php
+++ 06- Web Dynamique/01- JavaScript/04-Formulaire/Formulaire.php
@@ -13,7 +13,7 @@
         <div class="champs">
             <label>Nom :</label>
             <div class="espace"></div>
-            <input type="text" id="nom" required/>
+            <input type="text" id="nom" required pattern="[A-Za-z-' ]{3,}"/>
             <div class="image">
                 <img class="imgConseil" src="./intero.png" alt="">
                 <div class="conseil">Votre nom sans caractères spéciaux</div>
@@ -25,7 +25,7 @@
         <div class="champs">
             <label>Code postal :</label>
             <div class="espace"></div>
-            <input type="input" id="postal" required title="5 chiffres attendue"/>
+            <input type="input" id="postal" required title="5 chiffres attendue" pattern="\d{5}"/>
             <div class="image">
                 <img class="imgConseil" src="./intero.png" alt="">
                 <div class="conseil">Votre code postal, il contient 5 chiffres</div>
@@ -37,13 +37,37 @@
         <div class="champs">
             <label>Date de naissance :</label>
             <div class="espace"></div>
-            <input type="date" id="DDN" required/>
+            <input type="date" id="DDN" required pattern="[1-2][0-9][0-9][0-9]\-[0-1]?[0-9]\-[0-3]?[0-9]"/>
             <div class="image">
                 <img class="imgConseil" src="./intero.png" alt="">
                 <div class="conseil">Votre date de naissance au format jj/mm/aaaa</div>
                 <img class="imgErreur" src="" alt="">
             </div>
             <div class="erreur" id="erreurDDN"></div>
+        </div>
+        <div class="espaceHor"></div>
+        <div class="champs">
+            <label>E-mail :</label>
+            <div class="espace"></div>
+            <input type="texte" id="mail" required pattern="[\w]*\.?[\w-]+@[\w]+\.[a-z]{2,4}"/>
+            <div class="image">
+                <img class="imgConseil" src="./intero.png" alt="">
+                <div class="conseil">Votre adresse mail</div>
+                <img class="imgErreur" src="" alt="">
+            </div>
+            <div class="erreur" id="erreurMail"></div>
+        </div>
+        <div class="espaceHor"></div>
+        <div class="champs">
+            <label>Mot de passe :</label>
+            <div class="espace"></div>
+            <input type="password" id="mdp" required pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[!@#\$%\^&\*+])[a-zA-Z\d!@#\$%\^&\*+]{8,}"/>
+            <div class="image">
+                <img class="imgConseil" src="./intero.png" alt="">
+                <div class="conseil gros">Il doit contenir au moins 8 caractères dont une majuscule, une minuscule, un caratère spécial et un chiffre</div>
+                <img class="imgErreur" src="" alt="">
+            </div>
+            <div class="erreur" id="erreurMdp"></div>
         </div>
         <div class="espaceHor"></div>
         <div class="champs">
